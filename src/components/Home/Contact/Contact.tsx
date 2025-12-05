ec75191f4017c7128db44b5f4e35f33876d3066d
Radwan Fahim <88280947+radwanfahim@users.noreply.github.com>
2025-12-05 18:14:26 +0600

    form added

--- src/components/Home/Contact/Contact.tsx
+++ src/components/Home/Contact/Contact.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
           </p>
         </div>
 
-        <div class="mt-10 ">
+        <div class="mt-10 grid grid-cols-1 lg:grid-cols-2 gap-8">
           {/* get ing touch */}
           <Info />
 
--- src/components/Home/Contact/Form.tsx
+++ src/components/Home/Contact/Form.tsx
@@ -1,5 +1,53 @@
+import { For } from "solid-js";
+import ServiceData from "../../../api/ServiceData";
+
 const Form = () => {
-  return <div></div>;
+  return (
+    <div class="">
+      <form>
+        {/* name */}
+        <fieldset class="fieldset ">
+          <legend class="fieldset-legend font-bold text-md">Your Name</legend>
+          <input
+            type="text"
+            class="input w-full rounded-md"
+            placeholder="User Name"
+            required
+          />
+
+          {/* email */}
+
+          <legend class="fieldset-legend font-bold text-md">
+            Email Address
+          </legend>
+          <input
+            type="email"
+            class="input w-full rounded-md"
+            placeholder="user@mail.com"
+            required
+          />
+
+          {/*Service Interested In*/}
+          <div>
+            <legend class="fieldset-legend font-bold text-md">
+              Service Interested In
+            </legend>
+            <select class="select w-full">
+              <option disabled selected>
+                Select a Service
+              </option>
+
+              <For each={ServiceData}>
+                {(service) => {
+                  return <option>{service?.title}</option>;
+                }}
+              </For>
+            </select>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
